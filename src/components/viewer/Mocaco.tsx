0a0ddb063908aa2589afc2da40203fb50b378592
Ryu Juheon <saidbysolo@gmail.com>
2024-01-14 03:18:37 +0000

    feat: change color

--- src/components/viewer/Mocaco.tsx
+++ src/components/viewer/Mocaco.tsx
@@ -4,7 +4,7 @@ import rosePineMoonTheme from './theme';
 
 const log = `2024/01/11 22:25:54 > steve(12345678-1234-5678-abcd-ef1234567801) snake_case at Test because VIEWING
 2024/01/11 22:25:54 > Herobrine(12345678-1234-5678-abcd-ef1234567801) camelCase at Make because GIVING
-2024/01/11 22:25:54 > _Alex_(12345678-1234-5678-abcd-ef1234567801l) string at Show because OFFERING
+2024/01/11 22:25:54 > _Alex_(12345678-1234-5678-abcd-ef1234567801) string at Show because OFFERING
 `
 
 const LogViewer = () => {
@@ -30,18 +30,16 @@ const LogViewer = () => {
             tokenizer: {
                 root: [
                     [/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/, "variable.parameter"],
-                    [/(\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2})/, "entity.name.section"],
+                    [/(\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2})/, "constant.numeric"],
                     [/\b(at|because)\b/, 'keyword'],
-                    [/\b[a-zA-Z_]\w*\b(?=\s*\()/, 'entity.name'],
+                    [/\b[a-zA-Z_]\w*\b(?=\s*\()/, 'support.function'],
                     [/\b[A-Z]\w*\b/, 'entity.name.tag'], // upper case
-                    [/\b[a-z]\w*\b/, 'meta.tag'], // lower case
-                    [/\b[a-zA-Z][a-z0-9]*([A-Z][a-z0-9]*)*\b/, 'meta.tag'], // camelCase 
+                    [/\b[a-z]\w*\b/, 'string'], // lower case
+                    [/\b[a-zA-Z][a-z0-9]*([A-Z][a-z0-9]*)*\b/, 'string'], // camelCase 
                 ],
 
             }
         })
-
-
 
         monaco.editor.setTheme('rosePineMoon')
     }, [monaco])
